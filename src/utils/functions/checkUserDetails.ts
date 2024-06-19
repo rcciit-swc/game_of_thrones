09f733f21b8975bb989517c8b3eccf64db24e850
Soumyaraj Bag <soumyarajbag@gmail.com>
2024-06-19 15:11:44 +0530

    add:fix-middleware

--- src/utils/functions/checkUserDetails.ts
+++ src/utils/functions/checkUserDetails.ts
@@ -1,7 +1,7 @@
 export const checkUserDetails = (data: Record<string, any>) => {
   let isSatisfied = true;
   Object.keys(data || {}).forEach((key) => {
-    if (!data[key]) {
+    if (key !== "college_roll" && (data[key] == null || data[key] === "" || data[key] === undefined)) {
       isSatisfied = false;
     }
   });
